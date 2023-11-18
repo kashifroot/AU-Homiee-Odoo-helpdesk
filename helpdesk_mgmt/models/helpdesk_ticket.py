dddde826e70808391e1b14e497a54479d1349a95
raoRocket <muhammad.kashif@skyrocket.com.pk>
2023-11-19 02:41:22 +0500

    initial

--- helpdesk_mgmt/models/helpdesk_ticket.py
+++ helpdesk_mgmt/models/helpdesk_ticket.py
@@ -259,13 +259,27 @@ class HelpdeskTicket(models.Model):
             return recipients
         return recipients
 
-    def _notify_get_reply_to(self, default=None):
-        """Override to set alias of tasks to their team if any."""
-        aliases = self.sudo().mapped("team_id")._notify_get_reply_to(default=default)
-        res = {ticket.id: aliases.get(ticket.team_id.id) for ticket in self}
-        leftover = self.filtered(lambda rec: not rec.team_id)
-        if leftover:
-            res.update(
-                super(HelpdeskTicket, leftover)._notify_get_reply_to(default=default)
-            )
-        return res
+    # def _notify_get_reply_to(
+    #     self, default=None, records=None, company=None, doc_names=None
+    # ):
+    #     if self:
+    #         """Override to set alias of tasks to their team if any."""
+    #         aliases = (
+    #             self.sudo()
+    #             .mapped("team_id")
+    #             ._notify_get_reply_to(
+    #                 default=default, records=None, company=company, doc_names=None
+    #             )
+    #         )
+    #         res = {ticket.id: aliases.get(ticket.team_id.id) for ticket in self}
+    #         leftover = self.filtered(lambda rec: not rec.team_id)
+    #         if leftover:
+    #             res.update(
+    #                 super(HelpdeskTicket, leftover)._notify_get_reply_to(
+    #                     default=default, records=None, company=company, doc_names=doc_names
+    #                 )
+    #             )
+    #         return res
+    #     else:
+    #         return  super(HelpdeskTicket, self)._notify_get_reply_to(
+    #                     default=default, records=None, company=company, doc_names=doc_names)
